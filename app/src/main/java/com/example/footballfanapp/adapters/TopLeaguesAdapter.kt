de2e00de3f176a456cfe9a34077925511291a357
Mathew <mateusz.laskowski@hotmail.com>
2021-03-01 18:01:07 +0100

    Create TopLeaguesAdapter

---
+++ app/src/main/java/com/example/footballfanapp/adapters/TopLeaguesAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.footballfanapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.footballfanapp.databinding.TopLeaguesRowLayoutBinding
+import com.example.footballfanapp.models.Competition
+import com.example.footballfanapp.models.TopLeaguesModel
+
+class TopLeaguesAdapter : RecyclerView.Adapter<TopLeaguesAdapter.MyViewHolder>() {
+
+    private var topLeagues = emptyList<Competition>()
+
+    class MyViewHolder(private val binding: TopLeaguesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(competition: Competition) {
+            binding.competition = competition
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TopLeaguesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentCompetition = topLeagues[position]
+
+        holder.bind(currentCompetition)
+    }
+
+    override fun getItemCount(): Int {
+       return topLeagues.size
+    }
+
+    fun setData(newData: TopLeaguesModel) {
+        topLeagues = newData.competitions
+        notifyDataSetChanged()
+    }
+
+}
